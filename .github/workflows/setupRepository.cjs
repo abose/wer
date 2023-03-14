41763cc7e178a1ea7209b3b447b5a22bb7e3e518
Arun Bose <tellarunbose@gmail.com>
2023-03-14 12:40:32 +0530

    dfg

--- .github/workflows/setupRepository.cjs
+++ .github/workflows/setupRepository.cjs
@@ -52,10 +52,10 @@ async function initRepo(details){
     repoName = details.repoName;
     console.log("github: ", JSON.stringify(github, null, 4));
     console.log("context: ", JSON.stringify(context, null, 4));
-    // if(!_isValidRepoInitEvent()){
-    //     console.log("Not a valid repo creation event. This task is only meant to be executed at repo creation. Exiting!");
-    //     return;
-    // }
+    if(!_isValidRepoInitEvent()){
+        console.log("Not a valid repo creation event. This task is only meant to be executed at repo creation. Exiting!");
+        return;
+    }
     setupPackageJSON();
     verifyRepoIsPublic();
     
